0134db4fc1bbb43343aeea273dac8e41fd126772
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-04-30 19:07:40 +0545

    Mongo db connected

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
+
+//load env vars
+dotenv.config({path: './config/config.env'});
+
+//connect db 
+connectDB();
+
 
 //Routes files
 const bootcamps = require('./routes/bootcamps');
 
-//load env vars
-dotenv.config({path: './config/config.env'});
 
 const app = express();
 
@@ -23,4 +28,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server runing in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server runing in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
