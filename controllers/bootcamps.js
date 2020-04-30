740c2d1d0e319659c4b6c101f849598f85b0130e
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-04-30 18:26:44 +0545

    middleware added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,44 +2,41 @@
 //@route GET /api/v1/bootcamps
 //@access Public
 exports.getBootcamps = (req, res, next) => {
-    res
+  res
     .status(200)
-    .json({success: true, msg: 'Show all bootcamps'});
-}
+    .json({ success: true, msg: "Show all bootcamps",});
+};
 
 //@desc  Get single bootcamps
 //@route GET /api/v1/bootcamps/:id
 //@access Public
 exports.getBootcamp = (req, res, next) => {
-    res
+  res
     .status(200)
-    .json({success: true, msg: `Show bootcamp ${req.params.id}`});
-
-}
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
 
 //@desc  Create new bootcamp
 //@route POST /api/v1/bootcamps
 //@access Private
 exports.createBootcamp = (req, res, next) => {
-    res
-    .status(200)
-    .json({success: true, msg: 'Create new bootcamps'});
-}
+  res.status(200).json({ success: true, msg: "Create new bootcamps" });
+};
 
 //@desc  Update bootcamp
 //@route PUT /api/v1/bootcamps/:id
 //@access Private
 exports.updateBootcamp = (req, res, next) => {
-    res
-      .status(200)
-      .json({success: true, msg: `Update bootcamps ${req.params.id}`});
-}
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+};
 
 //@desc  Delete  bootcamp
 //@route DELETE /api/v1/bootcamps/:id
 //@access Private
 exports.deleteBootcamp = (req, res, next) => {
-    res.status(200)
-    .json({success: true, msg: `Delete bootcamp ${req.params.id}`});
-
-}
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Routes files
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +10,12 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
