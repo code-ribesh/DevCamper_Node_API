da02f71dd9afe63d280c3edcd00952fc9538d551
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-04-30 18:02:13 +0545

    Routes added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Routes files
+const bootcamps = require('./routes/bootcamps');
+
 //load env vars
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 
 const PORT = process.env.PORT || 5000;
 
