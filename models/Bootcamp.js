89cce720a5664694eb88e15f73b2a68e665233f0
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-05-01 12:23:17 +0545

    GeoJson middleware added

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require('slugify');
+const geocoder = require('../utils/geocode');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -106,5 +107,28 @@ BootcampSchema.pre('save', function(next){
   this.slug = slugify(this.name, {lower: true});
   next();
 });
+
+
+//Geocode & create location field
+BootcampSchema.pre('save' ,async function(next){
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates:[
+      loc[0].longitude, loc[0].latitude
+    ],
+    formattedAddress:loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+ }
+
+ //do not save address in db
+ this.address = undefined;
+  next();
+});
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
