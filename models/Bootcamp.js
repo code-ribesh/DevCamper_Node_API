a8ec2d973bc5789ab308bf0cf8a7ce1156fd557c
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-05-01 12:03:46 +0545

    SLugify added

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,6 +98,13 @@ const BootcampSchema = new mongoose.Schema({
     default: Date.now
   },
   
+});
+
+
+//create bootcamp slug from the name
+BootcampSchema.pre('save', function(next){
+  this.slug = slugify(this.name, {lower: true});
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
