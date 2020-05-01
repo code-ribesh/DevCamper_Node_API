db671bf4b630bf9ffee410d3c1df5103d9058cae
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-05-01 15:11:26 +0545

    Added Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   // fileds to excute
 
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // Copy req.query
   const reqQuery = {...req.query};
@@ -44,12 +44,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  //pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+
   //excueting query
   const bootcamps = await query;
-  //const bootcamps = await Bootcamp.find();
+  //Pagination result
+  const pagination = {};
+  if(endIndex < total){
+   pagination.next = {
+     page: page + 1,
+     limit
+   }
+  }
+
+  if(startIndex > 0){
+     pagination.prev = {
+       page: page -1,
+       limit
+     }
+  }
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
